efc5ea09823b109cb05e4e92861700be188d35a8
Ihza Luckyan Passya <ipassya@gmail.com>
2022-01-18 20:16:29 +0700

    Add Challenge

---
+++ 03 - Challenge/script.js
@@ -0,0 +1,70 @@
+// 1
+let a = 50;
+if (a == 50) {
+  while (a) {
+    console.log(a);
+    a--;
+  }
+} else {
+  console.log(true);
+}
+
+// 2
+let a = 50;
+let b = 10;
+if (b + a != 32) {
+  while (b) {
+    console.log(b);
+    b--;
+  }
+} else {
+  console.log(true);
+}
+
+// 3
+let a = 10;
+let c = 18;
+if (c + a == 28) {
+  while (c) {
+    console.log(c);
+    c--;
+  }
+} else {
+  console.log(true);
+}
+
+// 4
+let x = 35;
+if (x == 35) {
+  while (x) {
+    console.log(x);
+    x--;
+  }
+} else {
+  console.log(true);
+}
+
+// 5
+let x = 50;
+let y = 100;
+if (y + x != 35) {
+  while (y) {
+    console.log(y);
+    y--;
+  }
+} else {
+  console.log(true);
+}
+
+// 6
+let c = 50;
+let y = 100;
+let a = 100;
+if (c + y + a != 25) {
+  while (a) {
+    console.log(a);
+    a--;
+  }
+} else {
+  console.log(true);
+}
